3e0268b2f3b2f5f922271d0424a796f633972391
TURuibo <ruibo.tu@gmail.com>
2019-07-16 15:14:49 +0200

    bug free generation and test-wise PC

--- run_me.R
+++ run_me.R
@@ -7,33 +7,42 @@ data_path<-paste(proj_path,'/data',sep="")
 source(paste(src_path,'/all_functions.R',sep=""))
 
 # ********* Synthethic data generation ********* 
-num_var = 10
-num_sample = 10000
-rdm_seed = 100
+num_var = 20
+num_sample = 100000
+rdm_seed = 10
 
-gen_result_list<-gen_data(num_var,num_sample,"mar",rdm_seed)
+gen_result_list<-gen_data(num_var,num_sample,"mnar",rdm_seed)
 
 data_complete = gen_result_list$data_complete
 data_m = gen_result_list$data_m
 data_ref = gen_result_list$data_ref
 myCPDAG = gen_result_list$ground_truth$cpdag
+collider = gen_result_list$ground_truth$collider
+prt = gen_result_list$ground_truth$parent_m_ind
+
+print(collider)
+print(prt) 
+print(gen_result_list$ground_truth$m_ind)
 
 # ********* Test-wise deletion PC ********* 
-suffStat <- list(data=data_complete)
-dag <- pc(suffStat, gaussCItest_tw_del, alpha=0.01, p=num_var)
+suffStat_complete <- list(data=data_complete)
+dag <- pc(suffStat_complete, gaussCItest_tw_del, alpha=0.01, p=num_var)
 shd_comp = shd(dag, myCPDAG)
 
-suffStat <- list(data=data_ref)
-dag <- pc(suffStat, gaussCItest_tw_del, alpha=0.01, p=num_var)
+suffStat_ref <- list(data=data_ref)
+suffStat_ref_lw_del<- list(data=test_wise_deletion(1:num_var, data_ref))
+dag <- pc(suffStat_ref_lw_del, gaussCItest_tw_del, alpha=0.01, p=num_var)
 shd_ref = shd(dag, myCPDAG)
 
-suffStat <- list(data=data_m)
-dag <- pc(suffStat, gaussCItest_tw_del, alpha=0.01, p=num_var)
+suffStat_m <- list(data=data_m)
+suffStat_m_lw_del<- list(data=test_wise_deletion(1:num_var, data_m))
+dag <- pc(suffStat_m_lw_del, gaussCItest_tw_del, alpha=0.01, p=num_var)
 shd_m = shd(dag, myCPDAG)
 
 print(shd_comp)
 print(shd_ref)
 print(shd_m)
+
 
 # ********* Missing Value PC (MVPC) ********* 
 # Detecting variables with missing values in the data set
--- src/all_functions.R
+++ src/all_functions.R
@@ -1,5 +1,5 @@
 library(pcalg)
-library(Rfast)
+library(mvtnorm)
 
 #******************Functions for synthetic data generation******************
 gen_data <- function(p,n,mode='mar',seed=1000){
@@ -13,23 +13,23 @@ gen_data <- function(p,n,mode='mar',seed=1000){
   # data_ref: the MCAR data set as reference 
   # ground_truth: the ground-true DAG, CPDAG, 
   
+  set.seed(seed) # one seed has a corresponding random graph (seed controls the graph)
   myDAG <- randomDAG(p,2/(p-1))
   ## Make sure the graph contains collider
   # while(!check_collider(myDAG)){
   #   myDAG <- randomDAG(p,2/(p-1))
   # }
-  set.seed(seed)
-  data_complete <- rmvnorm(n, mu=rep(0,p), sigma=trueCov(myDAG))
   
   # Make sure the collider is the cause of R-variable of its parent variable
-  data_del <-del(data_complete,myDAG,mode)
-  data_m = data_del$data_mar
-  
-  # Also provide reference, a MCAR data set and the ground-truth (DAG, missingness indicator, and parents of the missingness indicators ).
+  data_del <-gen_del(p,n,myDAG,mode)
+  data_m = data_del$data_m
+  data_complete = data_del$data_complete
   data_ref <- data.frame(data_del$data_ref)
+  # Also provide reference, a MCAR data set and the ground-truth (DAG, missingness indicator, and parents of the missingness indicators ).
   ground_truth <- list(
     dag=myDAG,
     cpdag=dag2cpdag(myDAG),
+    collider=detect_colliders(myDAG),
     parent_m_ind=data_del$parent_m_ind,
     m_ind=data_del$m_ind
   )
@@ -40,7 +40,7 @@ gen_data <- function(p,n,mode='mar',seed=1000){
               ground_truth=ground_truth))
 }
 
-del <- function(data, myDAG,mode='mar',num_m=6){
+gen_del <- function(p,n,myDAG,mode='mar',num_m=6){
   # Constraint of MAR:
   # 1. the missingness indicator is caused by some substantive variables;
   # 2. the parents of missingness indicator have no missing values in the data set.
@@ -53,9 +53,8 @@ del <- function(data, myDAG,mode='mar',num_m=6){
   # 3. the limitation of the correction method 
   # ********************
   
-  size <- dim(data)
-  num_var <- size[2]
-  num_samples <- size[1]
+  num_var <- p
+  num_samples <- n
   # 1. Choose the missingness indicators/ choose which variables contain missing values 
   m_ind = select_m_ind(num_var,num_m)
   
@@ -67,11 +66,13 @@ del <- function(data, myDAG,mode='mar',num_m=6){
   }
   
   # 3. Generate missing values according the the values of missingness indicators
-  data_mar_ref = generate_missing_values(data,m_ind,prt_m)
-  data_mar = data_mar_ref$data_mar
-  data_ref = data_mar_ref$data_ref
+  data_com_m_ref = generate_missing_values(p,n,myDAG,m_ind,prt_m)
+  data_m = data_com_m_ref$data_m
+  data_ref = data_com_m_ref$data_ref
+  data_complete = data_com_m_ref$data_complete
   
-  return(list(data_mar=data_mar,
+  return(list(data_complete=data_complete,
+              data_m=data_m,
               data_ref=data_ref,
               parent_m_ind=prt_m,
               m_ind=m_ind)) 
@@ -96,23 +97,33 @@ select_prt_mnar_ind<-function(m_ind,num_var){
   return(prts)
 }
 
-generate_missing_values<-function(data,m_ind,parent_m_ind){
+generate_missing_values<-function(p,n,myDAG,m_ind,parent_m_ind){
   # Give the parents of missingness indicators, and the missingness indcators
   # The missing values are generated whn the parent values are in the bottom XX percentage.
-  
-  data_mar=data
+  data <- rmvnorm(n, mean=rep(0,p), sigma=trueCov(myDAG))
+  data_m=data
   data_mcar=data
   for(i in c(1:length(m_ind))){
     # Choose lower "bottom_p" percentage of the values
     bottom_p <- runif(1, min = 0.1, max = 0.5)
     r <- data[,parent_m_ind[i]] < qnorm(bottom_p)
-    data_mar[r==1, m_ind[i]] = NA
+    data_m[r==1, m_ind[i]] = NA
     r_mcar<-sample(r)
     data_mcar[r_mcar==1, m_ind[i]] = NA
   }
-  return(list(data_mar=data_mar,data_ref=data_mcar))
+  return(list(data_complete=data, 
+              data_m=data_m,
+              data_ref=data_mcar))
 }
 
+detect_colliders<-function(myDAG){
+  m <- as(myDAG,"matrix")
+  m[m>0]<-1
+  ind <- colSums(m,1)>1
+  num_vars<-length(m[,1])
+  x <- c(1:num_vars)
+  x[ind]
+}
 # Example of generating synthetic data
 # gen_data(20,10,"mnar")
 
